00cd7c93b0fa83801f30d88e39d120c4085e0a86
Matt Wilson <mwilson82@hotmail.com>
2022-11-17 21:52:10 +0000

    implement categories feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   
+
   root to: 'products#index'
 
   resources :about, only: [:index]
@@ -17,6 +18,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
