8447a7a9c78c9a484e1b7a2c20af21a06470ed8c
Matt Wilson <mwilson82@hotmail.com>
2022-11-23 00:24:07 +0000

    add test for cart feature

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -7,12 +7,9 @@ describe('jungle app', () => {
     cy.visit('/');
   });
 
-  it("There are products on the page", () => {
-    cy.get(".products article").should("be.visible");
-  });
-
-  it("There is 2 products on the page", () => {
-    cy.get(".products article").should("have.length", 2);
+  it('Changes the item count in the cart when the add-to-cart button is clicked', () => {
+    cy.get('.btn').first().click({ force: true });
+    cy.contains(".nav-link", "My Cart (1)");
   });
 
 });
